0f13e5143cc0ece14cf65dc57a1ba683b27debb5
Patrick Huynh <pldhuynh@gmail.com>
2020-06-27 00:25:25 +1000

    first working version

---
+++ src/component/Definitions.js
@@ -0,0 +1,230 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import stringSimilarity from "string-similarity";
+
+import stopwords from "../data/stopwords";
+import definitions from "../data/definitions";
+
+function Definitions(props) {
+  const [definitionArray, setDefinitionArray] = useState(definitions);
+  const [definition, setDefinition] = useState(
+    definitionArray[Math.floor(Math.random() * definitionArray.length)]
+  );
+  const [definitionState, setDefinitionState] = useState(0);
+  const [hintButtonText, setHintButtonText] = useState("Hint");
+  const [answerScore, setAnswerScore] = useState(0);
+  const [answer, setAnswer] = useState("");
+  const [powerLevel, setPowerLevel] = useState(0);
+  const [answerSubmitted, setAnswerSubmitted] = useState(false);
+
+  useEffect(() => {
+    getDefintitions();
+  }, []);
+
+  const getDefintitions = async () => {
+    // let url = "http://192.168.1.20:8529/_db/_system/api/get/all/objects";
+    // let result = await axios.get(url);
+    setDefinitionArray(definitions);
+  };
+
+  const handleNext = async () => {
+    let currentDefinitionIndex = definitionArray.indexOf(definition);
+    let nextDefinitionIndex = Math.floor(Math.random() * definitions.length);
+    let i = 0;
+    do {
+      nextDefinitionIndex = Math.floor(Math.random() * definitions.length);
+      i++;
+    } while (nextDefinitionIndex !== currentDefinitionIndex && i < 5);
+    setDefinition(definitions[Math.floor(Math.random() * definitions.length)]);
+    setDefinitionState(0);
+    setHintButtonText("Hint");
+    setAnswer("");
+    setAnswerScore(0);
+    setAnswerSubmitted(false);
+  };
+
+  const handleHint = () => {
+    setHintButtonText("Hint Again");
+    setDefinitionState(Math.max(1, definitionState + 1));
+  };
+
+  const handleShow = () => {
+    setDefinitionState(-1);
+  };
+
+  const handleSubmitAnswer = () => {
+    if (definition.definition ? (answer ? true : false) : false) {
+      let modifiedDefinition = definition.definition
+        .match(/\b(\w+)\b/g)
+        .join(" ")
+        .replace(/[^\w\s]|_/g, "")
+        .toLowerCase();
+      let modifiedAnswer = answer
+        .match(/\b(\w+)\b/g)
+        .join(" ")
+        .replace(/[^\w\s]|_/g, "")
+        .toLowerCase();
+      setAnswerScore(
+        stringSimilarity.compareTwoStrings(modifiedDefinition, modifiedAnswer)
+      );
+    } else {
+      setAnswerScore(0);
+    }
+    setAnswerSubmitted(true);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.charCode === 13) {
+    }
+  };
+
+  const scoreEncouragement = () => {
+    let encouragement;
+    if (answerScore < 0.5) {
+      encouragement = "Not quite right";
+    } else if (answerScore < 0.9) {
+      encouragement = "Check wording";
+    } else if (answerScore < 1) {
+      encouragement = "Getting closer!";
+    } else if (answerScore >= 1) {
+      encouragement = "Perfect!";
+    }
+    encouragement =
+      encouragement + " (Similarity: " + Math.floor(answerScore * 100) + "%)";
+    return encouragement;
+  };
+
+  const maskDefinition = (text, textState) => {
+    try {
+      if (textState >= 1) {
+        let words = text.match(/\b(\w+)\b/g);
+        let wordIndex;
+        let hintWords = textState;
+        for (wordIndex in words) {
+          if (stopwords.indexOf(words[wordIndex]) === -1) {
+            if (hintWords > 1) {
+              hintWords -= 1;
+            } else {
+              words[wordIndex] = "_".repeat(words[wordIndex].length);
+            }
+          }
+        }
+        return words.join(" ");
+      } else if (textState === 0) {
+        return "";
+      } else if (textState === -1) {
+        return text;
+      } else {
+        return "state not found";
+      }
+    } catch (e) {
+      return "";
+    }
+  };
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row mt-5">
+        <div className="col">
+          <h1>What's that defintion!?</h1>
+        </div>
+      </div>
+      <div
+        className="row justify-content-center align-items-center"
+        style={{ height: "100px" }}
+      >
+        <div className="col">
+          <h3>{definition.name}</h3>
+        </div>
+      </div>
+      <div className="row d-flex justify-content-center m-3">
+        <div className="col-8">
+          <textarea
+            className="form-control"
+            placeholder="Your answer.."
+            onChange={(e) => {
+              setAnswer(e.target.value);
+              setAnswerSubmitted(false);
+            }}
+            value={answer}
+          ></textarea>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-primary m-2" onClick={handleNext}>
+            Next Random Definition
+          </button>
+          <button
+            disabled={answer ? false : true}
+            className="btn btn-success m-2"
+            onClick={handleSubmitAnswer}
+          >
+            Submit Answer
+          </button>
+          <button
+            disabled={definition.name ? false : true}
+            className="btn btn-warning m-2"
+            style={{ width: "120px" }}
+            onClick={handleHint}
+          >
+            {hintButtonText}
+          </button>
+          <button
+            disabled={definition.name ? false : true}
+            className="btn btn-danger m-2"
+            onClick={handleShow}
+          >
+            Reveal
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <mark>(Scoring mechanism under development!)</mark>
+        </div>
+      </div>
+      <div
+        className="row m-5 justify-content-center align-items-center"
+        style={{ height: "100px" }}
+      >
+        <div className="col">
+          <div style={{ fontSize: "24px", fontFamily: "Lucida Console" }}>
+            {maskDefinition(definition.definition, definitionState)}
+          </div>
+        </div>
+      </div>
+      <div
+        className="row justify-content-center align-items-center"
+        style={{ height: "100px" }}
+      >
+        <div className="col">
+          {answerSubmitted ? <h1>{scoreEncouragement()}</h1> : <></>}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Definitions;
+
+/*
+
+      <div className="row">
+        {true ? (
+          <></>
+        ) : (
+          <table className="table table-sm text-left">
+            <tbody>
+              {defns.map((def) => (
+                <tr key={def.name}>
+                  <td>{def.name}</td>
+                  <td>{def.definition}</td>
+                  <td>{maskDefinition(def.definition)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+      */
---
+++ src/data/definitions.js
@@ -0,0 +1,99 @@
+const definitions = [
+  {
+    name: "reason",
+    definition: "that which is put forth as a reason",
+  },
+  {
+    name: "correct reason",
+    definition: "that which is the three modes",
+  },
+  {
+    name: "position property in proving that sound is impermanent",
+    definition:
+      "sound is the faultless subject of interest in proving that sound is impermanent by reason of this and it is ascertained by valid cognition to only exist, in a manner similar to the way it is put forth, in relation to sound, by means of being mutually distinct from sound",
+  },
+  {
+    name: "forward pervasion in proving that sound is impermanent",
+    definition:
+      "there is a correct similar example incorporating both reason and predicate in proving sound is impermanent by reason of this; this is related to impermanent; and this is ascertained by valid cognition to only exist, similar to the way it is put forth, for solely the similar class in proving sound is impermanent",
+  },
+  {
+    name: "reverse pervasion in proving that sound is impermanent",
+    definition:
+      "there is a correct dissimilar example lacking both reason and predicate in proving that sound is impermanent by reason of this; this is related to impermanent; and this is ascertained by valid cognition as just not existing for the dissimilar class in proving that",
+  },
+  {
+    name: "correct reason of effect",
+    definition:
+      "it is a correct affirming reason proving that; and there is a possible common locus between what is held to be the explicit predicate proving that by reason of it and its cause",
+  },
+  {
+    name: "correct reason of nature",
+    definition:
+      "a correct reason proving that and if something is held as the explicit predicate in proving that by reason of it, it is necessarily intrinsically one with it",
+  },
+  {
+    name: "correct reason of non-observation",
+    definition:
+      "it is a correct reason proving that; and a common locus between that held as the explicit predicate proving that by reason of it and a negation exists",
+  },
+  {
+    name: "consequence",
+    definition: "that which is put forth as a consequence",
+  },
+  {
+    name: "correct consequence",
+    definition:
+      "a consequence that projects an unaccepted consequence to another person by means of the reason and pervasion being established from the perspective of the mind of the other person and [for which] the opponent with respect to whom it is set forth is unable to arrive at a factually concordant answer",
+  },
+  {
+    name: "correct consequence that projects a proof",
+    definition: "correct consequence that fulfills the opposites three modes",
+  },
+  {
+    name: "correct consequence that does not project a proof",
+    definition:
+      "correct consequence that does not fulfill the opposites three modes",
+  },
+  {
+    name: "pseudo consequence",
+    definition:
+      "a stated consequence that is unable to suppress the manifest wrong conception that unequivocally grasps that is its object to be eliminated",
+  },
+  {
+    name: "established base",
+    definition: "that which is established by valid cognition",
+  },
+  {
+    name: "permanent",
+    definition: "a common locus of phenomenon and non-momentary",
+  },
+  {
+    name: "impermanent",
+    definition: "momentary",
+  },
+  {
+    name: "functioning thing",
+    definition: "that which is able to perform a function",
+  },
+  {
+    name: "matter",
+    definition: "established by particles",
+  },
+  {
+    name: "consciousness",
+    definition: "clear and knowing",
+  },
+  {
+    name: "non-associated composite",
+    definition: "a compound that is neither matter nor awareness",
+  },
+  {
+    name:
+      "faultless subject of interest proving that sound is impermanent by reason of it being a product",
+    definition:
+      "that which is observed to be a common locus between what is held to be the basis of dispute in proving that sound is impermanent by reason of it being a product; and being [that where] there can be a person who, having ascertained with valid cognition that it is a product, is interested in whether or not it is impermanent",
+  },
+];
+
+module.exports = definitions;
